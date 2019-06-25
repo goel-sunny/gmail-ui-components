55d1876f0cb2ebc2293db8d997f5dac51494c14c
Sunny Goel <sunny.goel@gemalto.com>
2019-06-25 10:34:44 +0530

    Navbar component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { SideBarComponent } from './core/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
